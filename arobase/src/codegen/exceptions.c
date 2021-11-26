5fc3223651335ad0ccf352b6dbae22054365160e
Njord <52111877+Njord0@users.noreply.github.com>
2021-11-26 15:49:29 +0100

    src: Update labels name for exceptions

--- arobase/src/codegen/exceptions.c
+++ arobase/src/codegen/exceptions.c
@@ -15,7 +15,7 @@ emit_try_block(Statement_t *stmt)
     emit("lea rdi, jmp_struct\n");
     emit("call setjmp\n");
     emit("cmp rax, 0\n"
-         "jne lbl%d\n",
+         "jne .LC%.5d\n",
          lbl
         );
 
@@ -26,13 +26,13 @@ emit_try_block(Statement_t *stmt)
 
     emit("call leave_exception\n");
 
-    emit("jmp lbl%d\n", lbl_after); // Jump after try block if no exception is raised
+    emit("jmp .LC%.5d\n", lbl_after); // Jump after try block if no exception is raised
 
-    emit("lbl%d:\n", lbl);
+    emit(".LC%.5d:\n", lbl);
 
     emit_statements(&stmt->else_block); // except block
 
-    emit("lbl%d:\n", lbl_after);
+    emit(".LC%.5d:\n", lbl_after);
 }
 
 void
@@ -45,9 +45,9 @@ emit_raise(Statement_t *stmt)
     emit("call leave_exception\n");
     emit("pop rax\n");
     emit("cmp rax, 0\n");
-    emit("jne lbl%d\n", lbl);
+    emit("jne .LC%.5d\n", lbl);
     emit("call exception\n");
-    emit("lbl%d:\n", lbl);
+    emit(".LC%.5d:\n", lbl);
     emit("mov rdi, rax\n");
     emit("mov rsi, 1\n");
     emit("call longjmp\n");
